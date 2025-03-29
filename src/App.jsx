94c321e698bba347349c4102e0339c27243c82ca
Vansh Bhatia <133893287+Vanshprocoder@users.noreply.github.com>
2025-03-29 21:01:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <ResumeAnalyzer />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MockInterviews />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
